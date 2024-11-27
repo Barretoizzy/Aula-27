d14aa9086450a213009eb1a760acc65855b7c6f4
izabele <izabele.matias@aluno.senai.br>
2024-11-27 11:07:37 -0500

    inicio do código

---
+++ cod.py
@@ -0,0 +1,8 @@
+nome = int(input("numero de alunos"))
+nota1 = float(input())
+nota2 = float(input())
+nota3 = float(input()) 
+nota4 = float(input())
+
+faltas = int(input("numero de faltas:10"))
+media = (nota1+nota2+nota3+nota4)/4
